02974d1daa0951f204de6e5c26866efe997ff18e
Alexan <alexanlima1.0@gmail.com>
2023-02-03 17:50:25 -0300

    Entendendo a funcionalidade do Property Biding

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -9,6 +9,9 @@ export class DataBidingComponent {
 
   public nome: String = "Roberto"
   public idade: number = 29
+  public checkedDesabled:boolean = true
+  public imgSRC: String = "https://cdn.akamai.steamstatic.com/steam/apps/105600/header.jpg?t=1666290860"
+
   constructor(){ }
 
 }
